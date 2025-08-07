df25261a2cc67825e417e03c092d736dc950562d
AndyOHT <00801279@red.unid.mx>
2025-08-07 01:39:21 -0600

    Agregado nuevos proyectos y foto de perfil

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import foto from '../assets/foto-perfil.jpg'
+import foto from '../assets/traje-perfil.jpg'
 
 const Perfil = () => (
   <section>
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -11,6 +11,30 @@ const Proyectos = () => (
     <p>Aplicación móvil diseñada para administrar floristerías de forma remota.</p>
     <p><strong>Tipo:</strong> App Móvil</p>
     <p><strong>Tecnologías:</strong> Kotlin, Android, Firebase, Firestore</p>
+
+    <h3>Biblioteca web</h3>
+    <p>Aplicación web para gestionar libros y accesos a la disponibilidad de los mismos.</p>
+    <p><strong>Tipo:</strong> Página Web</p>
+    <p><strong>Tecnologías:</strong> React, Vite, CSS, Firebase, Firestore</p>
+    <a href="https://bibliweb.netlify.app/" target="_blank" rel="noopener noreferrer">
+  <button className="custom-button">Web</button>
+</a>
+<a href="https://github.com/AndyOHT/bibli-web" target="_blank" rel="noopener noreferrer">
+  <button className="custom-button">Repositorio</button>
+</a>
+
+
+    <h3>Recursos Humanos</h3>
+    <p>En esta aplicación web nos concentramos en la integración del CRUD, al crear los usuarios, leer esta información desde Firebase/Firestore, actualizar datos de los empleados en tiempo real y también eliminarlos.</p>
+    <p><strong>Tipo:</strong> Página Web</p>
+    <p><strong>Tecnologías:</strong> React, Vite, CSS, Firebase, Firestore</p>
+    <a href="https://paginarrhh.netlify.app/" target="_blank" rel="noopener noreferrer">
+  <button className="custom-button">Web</button>
+</a>
+<a href="https://github.com/AndyOHT/web-rrhh" target="_blank" rel="noopener noreferrer">
+  <button className="custom-button">Repositorio</button>
+</a>
+
   </section>
 )
 
